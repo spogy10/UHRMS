23d5dd3bca770a5adb704d50911f9e11f73c1ae8
Stephen Mitchell <stevey982@gmail.com>
2019-03-11 02:32:59 -0400

    Add project files.

---
+++ UHRMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UHRMS.MvcApplication" Language="C#" %>
---
+++ UHRMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UHRMS.Startup))]
+namespace UHRMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
