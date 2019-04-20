c3e97b9a936ce1e77f13396cc95efa3847189890
Stephen Mitchell <stevey982@gmail.com>
2019-04-20 11:16:54 -0500

    Included Submit buttons and complaints class

---
+++ UHRMS/Models/Complaints.cs
@@ -0,0 +1,18 @@
+﻿namespace UHRMS.Models
+{
+    public class Complaints
+
+    {
+
+        public ApplicationUser Student { get; set; }
+
+        public int complainNo { get; set; }
+
+        public string subject { get; set; }
+
+        public string comments { get; set; }
+
+
+
+    }
+}
